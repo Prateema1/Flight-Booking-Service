d99dfb791505be65e83b660cd68c657978d2a9cc
Prateem1 <prateema.adhikari@gmail.com>
2024-01-30 18:40:09 +0530

    added booking logic with transactions

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -4,7 +4,6 @@ const { SuccessResponse, ErrorResponse } = require('../utils/common');
 
 async function createBooking(req, res) {
   try {
-    console.log(req.body);
    const flight = await BookingService.createBooking({
       flightId: req.body.flightId,
       userId: req.body.userId,

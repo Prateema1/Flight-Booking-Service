72ed977c326ed7db8d3952b8feb89fa16f19d383
Prateem1 <prateema.adhikari@gmail.com>
2024-01-30 19:56:39 +0530

    added cancel boooking logic

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -47,7 +47,7 @@ async function makePayment(data) {
     const bookingTime = new Date(bookingDetails.createdAt);
     const currentTime = new Date();
     if(currentTime - bookingTime > 300000) {
-      await bookingRepository.update(data.bookingId, {status: CANCELLED}, transaction);
+      await cancelBooking(data.bookingId);
       throw new AppError('The booking has expired', StatusCodes.BAD_REQUEST);
     }
   
@@ -58,13 +58,33 @@ async function makePayment(data) {
       throw new AppError('The user corresponding to the booking does not match', StatusCodes.BAD_REQUEST);
     }
     //we assume that payment is successful
-    const response = await bookingRepository.update(data.bookingId, {status: BOOKED}, transaction);
+    await bookingRepository.update(data.bookingId, {status: BOOKED}, transaction);
     await transaction.commit();
 
   } catch (error) {
     await transaction.rollback();
     throw error;
 
+  }
+}
+
+async function cancelBooking(bookingId) {
+  const transaction = await db.sequelize.transaction();
+  try {
+    const bookingDetails = await bookingRepository.get(bookingId, transaction);
+    if(bookingDetails.status === CANCELLED) {
+      await transaction.commit();
+      return true;
+    }
+    await axios.patch(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${bookingDetails.flightId}/seats`, {
+      seats: bookingDetails.noOfSeats,
+      dec: 0
+    });
+    await bookingRepository.update(bookingId, {status: CANCELLED}, transaction);
+    await transaction.commit();
+  } catch (error) {
+    await transaction.rollback();
+    throw error;
   }
 }
 
